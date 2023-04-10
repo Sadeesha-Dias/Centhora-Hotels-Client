6757cc8a3d251658ada3fcc981933ee0fe27ea91
Sadeesha-Dias <sddias19981230@gmail.com>
2023-04-10 07:22:14 +0530

    Created Routes for components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+
+import Home from './components/home';
+import Navbar from './components/navbar';
+import Footer from './components/footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <React.Fragment>
+        <Navbar />
+        <br />
+        <Routes>
+          <Route exact path="/" element={<Home/>} />
+          {/* <Route path="/centhora/user/login" element={<UserLogin/>} />
+          <Route path="/centhora/user/register" element={<UserRegistration/>} />
+          <Route path="/user/:username" element={<ViewUser/>} />
+          <Route path="/create/newbooking" element={<NewBooking/>} />
+          <Route path="/centhora/about-us" element={<AboutUs/>} />
+          <Route path="/centhora/contact-us" element={<ContactUs/>} /> */}
+        </Routes>
+        <Footer />
+      </React.Fragment>
+    </Router>
   );
 }
 
